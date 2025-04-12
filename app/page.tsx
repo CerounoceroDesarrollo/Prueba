daaa1d0ce7dd65e66c1bdde2de777e31867da2d3
CerounoceroDesarrollo <desarrollo010corp@gmail.com>
2025-04-11 19:38:45 -0600

    A ver

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <Profile01 
-        name="El Negaciano"
+        name="Tengo hambre"
         role="Desarrolladora Frontend"
         avatar="/placeholder.svg?height=300&width=300"
         subscription="Pro"
